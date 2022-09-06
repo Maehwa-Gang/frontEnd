d5d9dbb3e60cdc427a2673ba5cd71beb6c333117
Simin-Kim <show3391@gmail.com>
2022-09-06 19:14:17 +0900

    Feat: 라우터 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'normalize.css'
+import LoginPage from './pages/login/LoginPage';
+import Common from './components/Common/common';
 import BaseForm from './components/baseForm';
 
 
 function App() {
   return (
-    <>
-      <BaseForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<BaseForm />}></Route>
+        <Route path='/login' element={<LoginPage/>}></Route>
+        <Route path='/common' element={<Common/>}></Route>  {/* 컴포넌트 디자인용 */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
