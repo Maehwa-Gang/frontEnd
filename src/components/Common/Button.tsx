bfe21775bdcd5d243581d94b76dc6c72e6b8a838
Simin-Kim <show3391@gmail.com>
2022-09-07 19:03:17 +0900

    Feat: Props 및 컴포넌트 핸들러 추가

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,38 +1,46 @@
 /** @jsxImportSource @emotion/react */
 
-// 버튼 배경 색상
-const bgColors: {[key: string]: string} = {
-	default: "#5900CB",
-	white: "#FFFFFF"
-};
-// 폰트 색상
-const fontColors: {[key: string]: string} = {
-	default: "#FFFFFF",
-	white: "#000000"
-};
-// 버튼 border
-const btnBorders: {[key: string]: string} = {
-	default: "0",
-	white: "1px solid #000000"
+// 버튼 배경, 폰트, 테두리 색상
+const colors: {[key:string]: {}} = {
+	default: {
+		backgroundColor: "#5900CB",
+		color: "#FFFFFF",
+		border: "0"
+	},
+	white: {
+		backgroundColor: "#FFFFFF",
+		color: "#000000",
+		border: "1px solid #000000"
+	},
+	blue: {
+		backgroundColor: "#FFFFFF",
+		color: "#5900CB",
+		border: "1px solid #5900CB"
+	}
 }
-
-const Button = ({children, variant}: {children: any, variant: string}) => {
+interface ButtonProps {
+	children: any, 
+	variant: string, 
+	width: string,
+	handler: any
+}
+const Button = ({children, variant, width, handler}: ButtonProps) => {
 	return (
 		<button
 			css={{
-				width: '100%',
+				...colors[variant],
+				width: width,
 				height: '50px',
-				backgroundColor: bgColors[variant],
-				color: fontColors[variant],
 				display: 'flex',
 				alignItems: 'center',
 				justifyContent: 'center',
 				fontSize: '14px;',
 				fontWeight: '400',
+			    fontFamily: 'Spoqa Han Sans Neo',
 				borderRadius: '4px',
-				border: btnBorders[variant],
 				cursor: 'pointer'
 			}}
+			onClick={handler}
 		>
 			{children}
 		</button>
--- src/components/Common/Input.tsx
+++ src/components/Common/Input.tsx
@@ -1,13 +1,24 @@
 /** @jsxImportSource @emotion/react */
 
-const InputWrapper = ({children}: any) => {
+const InputWrappersChild: {[key: string]: {}} = {
+	// InputWrapper의 자식 요소가 2개일때 (input, button)
+	two: {
+		display: 'flex',
+    	justifyContent: 'space-between',
+    	alignItems: 'center',
+		border: '0'
+	}
+}
+const InputWrapper = ({children, elements}: {children: any, elements: string}) => {
 	return (
 		<div
 			css = {{
 				border: '1px solid #E7E7E7',
 				borderRadius: '4px',
 				fontWeight: '400',
-				fontSize: '14px'
+				fontSize: '14px',
+			    fontFamily: 'Spoqa Han Sans Neo',
+				...InputWrappersChild[elements]
 			}}
 		>
 			{children}
@@ -15,45 +26,71 @@ const InputWrapper = ({children}: any) => {
 	)
 }
 
-const DefaultInput = ({children, placeHolder, width}: {children: any, placeHolder: string, width: string}) => {
+interface TextInputProps {
+	children: any, 
+	placeHolder: string, 
+	width: string,
+	setInputState(a: string): void 
+}
+const TextInput = ({children, placeHolder, width, setInputState}: TextInputProps) => {
 	return (
-			<input placeholder={placeHolder}
+			<input placeholder={placeHolder} type='text'
 				css={{
 					width: width,
 					height: '50px',
 					border: '0',
 					padding: '13px'
 				}}
+				onChange={(e)=>setInputState(e.target.value)}
 			/>
 	)
 }
 
-const HideInput = ({children, placeHolder, width}: {children: any, placeHolder: string, width: string}) => {
+interface PwInputProps extends TextInputProps {
+	show: string,
+	setInputHide(a: string): void
+}
+const showContent: {[key: string]: string[]} = {
+	see: ['text', 'see'],
+	hide: ['password', 'hide']
+}
+const PwInput = ({children, placeHolder, width, show, setInputState, setInputHide}: PwInputProps) => {
 	return (
 		<div
 			css={{
+				width: width,
 				display: 'flex',
 				justifyContent: 'space-between',
-				alignItems: 'center'
+				alignItems: 'center',
+				border: '1px solid #E7E7E7',
+				borderRadius: '4px',
 			}}
 		>
-			<input placeholder={placeHolder}
+			<input placeholder={placeHolder} type={showContent[show][0]}
 				css={{
-					width: width,
+					width: '100%',
 					height: '50px',
 					border: '0',
 					padding: '13px'
 				}}
+				onChange={(e)=>setInputState(e.target.value)}
 			/>
-			<img src='/assets/images/common/icon_hide.svg'
+			<img src={`/assets/images/common/icon_${showContent[show][1]}.svg`}
 				css={{
 					paddingRight: '8px',
 					cursor: 'pointer'
 				}}
 				alt='비밀번호 보기'
+				onClick={() => {
+					if (show==='hide') {
+						setInputHide('see')
+					} else {
+						setInputHide('hide')
+					}
+				}}
 			/>
 		</div>
 	)
 }
 
-export {InputWrapper, DefaultInput ,HideInput}
+export {InputWrapper, TextInput ,PwInput}
