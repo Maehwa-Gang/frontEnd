f555f574e35e2bdd7fb1be6a5f93bca75d13a6f0
Simin-Kim <show3391@gmail.com>
2022-09-19 23:36:22 +0900

    Style: css 함수 styled 함수로 변경

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -9,13 +9,7 @@ import Button from "../../components/Common/Button";
 import { InputWrapper, TextInput, PwInput } from "../../components/Common/Input";
 import CheckBox from "../../components/Common/CheckBox";
 
-
-const LoginContainer = styled.div({
-	width: 'calc(100% - 26px)',
-	margin: '0 auto',
-	paddingTop: '63px'
-});
-
+/* styled-components */
 const LoginBannerLogo = styled.img({
 	margin: '0 auto',
 	display: 'block',
@@ -23,10 +17,31 @@ const LoginBannerLogo = styled.img({
 	height: '141px'
 })
 
+const LoginInputContainer = styled.div({
+  display:'flex', 
+  flexDirection:'column',
+  gap:'11px',
+  marginTop: '56px'
+})
+
+const CheckBoxWrapper = styled.div({
+  margin:'14px 0 36px 0'
+})
+
+const ButtonContainer = styled.div({
+  display:'flex',
+  flexDirection:'column',
+  gap:'11px'
+})
+
+/* React Components */
 const LoginPage = () => {
-  const [id, setId] = useState('')
-  const [pw, setPw] = useState('')
-	const [inputHide, setInputHide] = useState('hide')
+	const [userInputs, setUserInputs] = useState({
+    id: '',
+    pw: ''
+  });
+  const { id, pw } = userInputs;
+  const [inputHide, setInputHide] = useState('hide')
 	const [idSave, setIdSave] = useState(0)
 
   const loginHandler = (): void => {
@@ -37,27 +52,21 @@ const LoginPage = () => {
 	return (
 		<BaseForm>
 			<Global styles={global}/>
-			<LoginContainer>
-				<LoginBannerLogo src="/assets/images/common/main_logo.svg" alt="로고 이미지"/>
-				<div css={{display:'flex', flexDirection:'column', gap:'11px', marginTop: '56px'}}>
-					<InputWrapper elements='one'><TextInput placeHolder='아이디를 입력하세요.' width="100%" setInputState={setId}>''</TextInput></InputWrapper>
-					<InputWrapper elements='two'><PwInput placeHolder='비밀번호를 입력하세요.' width="100%" show={inputHide} setInputState={setPw} setInputHide={setInputHide}>''</PwInput></InputWrapper>
-				</div>
-				<div css={{margin:'14px 0 36px 0'}}>
-					<label onClick={()=>{setIdSave(1-idSave)}}><CheckBox active={idSave} margin='8px'>아이디 저장</CheckBox></label>
-				</div>
-				<div css={{display:'flex', flexDirection:'column', gap:'11px',}}>
-					<Button variant='default' width="100%" handler={()=>{loginHandler()}}>로그인</Button>
-					<Button variant='white' width="100%" handler=''>회원가입</Button>
-				</div>
-				<div>
-					{/* 소셜 로그인 */}
-				</div>
-			</LoginContainer>
-			{/* <Button variant='default' width="100%" handler={()=>{loginHandler()}}>로그인</Button>
-			<InputWrapper elements='one'><TextInput placeHolder='아이디를 입력하세요.' width="100%" setState={setId}>''</TextInput></InputWrapper>
-			<InputWrapper elements='two'><PwInput placeHolder='비밀번호를 입력하세요.' width="80%" show='hide' setState={setPw}>''</PwInput><Button variant='default' width='20%' handler=''>로그인</Button></InputWrapper>
-			<label><input type='checkbox' css={{display:'none'}} onClick={()=>console.log(1)}/><CheckBox active='yes' margin='8px'>아이디 저장</CheckBox></label> */}
+      <LoginBannerLogo src="/assets/images/common/main_logo.svg" alt="로고"/>
+      <LoginInputContainer>
+        <InputWrapper><TextInput name='id' placeHolder='아이디를 입력하세요.' inputs={userInputs} setInputsState={setUserInputs}>''</TextInput></InputWrapper>
+        <InputWrapper elements='button'><PwInput name='pw' placeHolder='비밀번호를 입력하세요.' inputs={userInputs} show={inputHide} setInputsState={setUserInputs} setInputHide={setInputHide}>''</PwInput></InputWrapper>
+      </LoginInputContainer>
+      <CheckBoxWrapper>
+        <label onClick={()=>{setIdSave(1-idSave)}}><CheckBox active={idSave}>아이디 저장</CheckBox></label>
+      </CheckBoxWrapper>
+      <ButtonContainer>
+        <Button width="100%" handler={()=>{loginHandler()}}>로그인</Button>
+        <Button variant='white' width="100%" handler=''>회원가입</Button>
+      </ButtonContainer>
+      <div>
+        {/* 소셜 로그인 */}
+      </div>
 		</BaseForm>
 	)
 }
