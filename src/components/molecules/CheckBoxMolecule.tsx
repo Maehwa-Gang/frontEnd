bffe297bd52eaa901d4156c8e677300a16f7828f
Simin-Kim <show3391@gmail.com>
2022-12-02 00:03:21 +0900

    Feat: 버튼 있는 체크박스 molecule 구현

--- src/components/molecules/CheckBoxMolecule.tsx
+++ src/components/molecules/CheckBoxMolecule.tsx
@@ -2,13 +2,17 @@
 
 import styled from "@emotion/styled"
 import CheckBox from "../atoms/CheckBox"
+import LinkArrow from "../atoms/LinkArrow"
 
 type CheckBoxContainerType = {
     margin: string
 }
 
 const CheckBoxContainer = styled.div<CheckBoxContainerType>`
-    margin: ${(props) => props.margin}
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: ${(props) => props.margin}    
 `
 
 interface CheckBoxMoleculeProps {
@@ -16,7 +20,11 @@ interface CheckBoxMoleculeProps {
     margin?: string,
     checkBoxHandler: any,
     active: number,
-    checkBoxMargin?: string
+    checkBoxMargin?: string,
+}
+
+interface CheckBoxWithArrowButtonMoleculeProps extends CheckBoxMoleculeProps {
+    arrowHandler: any
 }
 
 const CheckBoxMolecule = ({children, margin, checkBoxHandler, active, checkBoxMargin}: CheckBoxMoleculeProps) => {
@@ -30,4 +38,16 @@ const CheckBoxMolecule = ({children, margin, checkBoxHandler, active, checkBoxMa
     )
 }
 
-export default CheckBoxMolecule
+const CheckBoxWithArrowButtonMolecule = ({children, margin, checkBoxHandler, active, checkBoxMargin, arrowHandler}: CheckBoxWithArrowButtonMoleculeProps) => {
+
+    return (
+        <CheckBoxContainer margin={margin ? margin : '0'}>
+            <label onClick={ () => checkBoxHandler()} style={{display:'block'}}>
+                <CheckBox active={ active } margin={checkBoxMargin}>{children}</CheckBox>
+            </label>
+            <LinkArrow handler={ arrowHandler }></LinkArrow>
+        </CheckBoxContainer>
+    )
+}
+
+export { CheckBoxMolecule, CheckBoxWithArrowButtonMolecule }
