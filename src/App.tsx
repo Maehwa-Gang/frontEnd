1cae3b2e797925e5c4fb2f8f7f3285c34a12c932
Simin-Kim <show3391@gmail.com>
2022-12-02 00:01:53 +0900

    Feat: 약관 컴포넌트 nested routes에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import LoginPage from './pages/login/login';
 import Common from './components/common';
 import Agree from './pages/signIn/agree';
 import SignIn from './pages/signIn/signIn';
+import Policy from './pages/signIn/policy';
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path='/common' element={<Common/>}></Route>  {/* 컴포넌트 디자인용 */}
-          <Route path='/login' element={<LoginPage/>}></Route>
-          <Route path='/agree' element={<Agree/>}></Route>
-          <Route path='/signIn' element={<SignIn/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+			<Route path='/common' element={<Common/>}></Route>  {/* 컴포넌트 디자인용 */}
+			<Route path='/login' element={<LoginPage/>}></Route>
+			<Route path='/agree' element={<Agree/>}>
+				<Route path='tos' element={<Policy/>}></Route>
+				<Route path='pic' element={<Policy/>}></Route>
+			</Route>
+			<Route path='/signIn' element={<SignIn/>}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
