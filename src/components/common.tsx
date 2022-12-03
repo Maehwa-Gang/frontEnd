f5df23ec996aedcbfea5616ec23e500292b1194d
Simin-Kim <show3391@gmail.com>
2022-12-03 18:26:43 +0900

    Fix: Props 동기화

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -16,7 +16,7 @@ const Common = () => {
     <BaseForm>
       <Button variant='default' width="100%" handler={clickHandler}>로그인</Button>
       <InputWrapper><TextInput>''</TextInput></InputWrapper>
-      <InputWrapper elements='button'><PwInput width="80%" show={inputHide} setInputHide={setInputHide}>''</PwInput><Button width='20%' handler=''>인증</Button></InputWrapper>
+      <InputWrapper elements='button'><PwInput width="80%" show={inputHide} setInputHide={setInputHide}>''</PwInput><Button width='20%' handler={()=>{}}>인증</Button></InputWrapper>
       <label onClick={()=>{console.log('setState or 함수 넣는 곳')}}><input type='checkbox' css={{display:'none'}}/><CheckBox>아이디 저장</CheckBox></label>
     </BaseForm>
   );
