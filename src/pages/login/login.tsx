c0b6d34e9172f13674a68ef48ee86f4dcacc3804
Simin-Kim <show3391@gmail.com>
2022-12-05 16:41:15 +0900

    Feat: 로그인 페이지 팝업 적용

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -8,10 +8,15 @@ import BaseForm from "../../components/molecules/BaseForm";
 import { CheckBoxMolecule } from "../../components/molecules/CheckBoxMolecule";
 import { Button, ButtonWrapper } from "../../components/atoms/Button";
 import { InputWrapper, TextInput, PwInput } from "../../components/atoms/Input";
+import Popup from "../../components/molecules/Popup";
+import { useAppDispatch, useAppSelector } from '../../store/config'
+import { setFlag, setPhrase } from '../../store/slices/popupSlice'
 
 /* React Components */
 const LoginPage = () => {
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const popup = useAppSelector((state) => state.popup);
 
     const [userInputs, setUserInputs] = useState({
         id: '',
@@ -21,20 +26,20 @@ const LoginPage = () => {
     const [inputHide, setInputHide] = useState('hide')
     const [idSave, setIdSave] = useState(0)
 
-    const loginHandler = (): void => {
+    const loginApi = () => {
         axios.post(
             'https://api.bbig.co.kr/api/login',
             {
-            id: id,
-            password: pw
+                id: id,
+                password: pw
             },
         )
         .then(res=>{
             if (res.data.success === true){
-            alert('로그인 성공')
+                alert('로그인 성공')
             }
             else {
-            alert('로그인 실패')
+                popupHandler('로그인 실패', res.data.msg)
             }
         })
         .catch(err=>{
@@ -42,43 +47,63 @@ const LoginPage = () => {
         })
     }
 
+    const loginHandler = (): void => {
+        if (id === '' || pw === '') {
+            popupHandler('로그인 실패', '아이디 또는 비밀번호를 확인하세요.');
+        }
+        else {
+            loginApi()
+        }
+    }
+
     const signUpHandler = (): void => {
         navigate('/agree')
     }
 
+    const popupHandler = (title: string, content: string): void => {
+        dispatch(setFlag(1));
+        dispatch(setPhrase({title, content}));
+    }
+
     return (
-        <BaseForm>
-            <S.LoginBannerLogo src="/assets/images/common/main_logo.svg" alt="로고"/>
-            <S.LoginInputContainer>
-                <InputWrapper><TextInput name='id' placeHolder='아이디를 입력하세요.' inputs={ userInputs } setInputsState={ setUserInputs }>''</TextInput></InputWrapper>
-                <InputWrapper elements='button'><PwInput name='pw' placeHolder='비밀번호를 입력하세요.' inputs={ userInputs } show={ inputHide } setInputsState={setUserInputs} setInputHide={setInputHide}>''</PwInput></InputWrapper>
-            </S.LoginInputContainer>
-            <CheckBoxMolecule margin="14px 0 36px 0" checkBoxHandler={ () => {setIdSave(1-idSave)} } active={ idSave }>
-                아이디 저장
-            </CheckBoxMolecule>
-            <S.ButtonContainer>
-                <Button width="100%" handler={ () => loginHandler() }>로그인</Button>
-                <Button variant='white' width="100%" handler={ () => signUpHandler() }>회원가입</Button>
-            </S.ButtonContainer>
-            <S.UserFindContainer>
-                <S.UserFind to='#'>아이디 찾기</S.UserFind>
-                <S.PwFind to='#'>비밀번호 찾기</S.PwFind>
-            </S.UserFindContainer>
-            <S.SnsLoginContainer>
-                <h4>SNS로그인</h4>
-                <S.SnsLoginList>
-                    <S.SnsLoginButtons>
-                        <S.GoogleLoginButton></S.GoogleLoginButton>
-                    </S.SnsLoginButtons>
-                    <S.SnsLoginButtons>
-                        <S.KakaoLoginButton></S.KakaoLoginButton>
-                    </S.SnsLoginButtons>
-                    <S.SnsLoginButtons>
-                        <S.AppleLoginButton></S.AppleLoginButton>
-                    </S.SnsLoginButtons>
-                </S.SnsLoginList>
-            </S.SnsLoginContainer>
-        </BaseForm>
+        <>
+            <BaseForm>
+                <S.LoginBannerLogo src="/assets/images/common/main_logo.svg" alt="로고"/>
+                <S.LoginInputContainer>
+                    <InputWrapper><TextInput name='id' placeHolder='아이디를 입력하세요.' inputs={ userInputs } setInputsState={ setUserInputs }>''</TextInput></InputWrapper>
+                    <InputWrapper elements='button'><PwInput name='pw' placeHolder='비밀번호를 입력하세요.' inputs={ userInputs } show={ inputHide } setInputsState={setUserInputs} setInputHide={setInputHide}>''</PwInput></InputWrapper>
+                </S.LoginInputContainer>
+                <CheckBoxMolecule margin="14px 0 36px 0" checkBoxHandler={ () => {setIdSave(1-idSave)} } active={ idSave }>
+                    아이디 저장
+                </CheckBoxMolecule>
+                <S.ButtonContainer>
+                    <Button width="100%" handler={ () => loginHandler() }>로그인</Button>
+                    <Button variant='white' width="100%" handler={ () => signUpHandler() }>회원가입</Button>
+                </S.ButtonContainer>
+                <S.UserFindContainer>
+                    <S.UserFind to='#'>아이디 찾기</S.UserFind>
+                    <S.PwFind to='#'>비밀번호 찾기</S.PwFind>
+                </S.UserFindContainer>
+                <S.SnsLoginContainer>
+                    <h4>SNS로그인</h4>
+                    <S.SnsLoginList>
+                        <S.SnsLoginButtons>
+                            <S.GoogleLoginButton></S.GoogleLoginButton>
+                        </S.SnsLoginButtons>
+                        <S.SnsLoginButtons>
+                            <S.KakaoLoginButton></S.KakaoLoginButton>
+                        </S.SnsLoginButtons>
+                        <S.SnsLoginButtons>
+                            <S.AppleLoginButton></S.AppleLoginButton>
+                        </S.SnsLoginButtons>
+                    </S.SnsLoginList>
+                </S.SnsLoginContainer>
+                {
+                    popup.flag ? <Popup/> : ''
+                }
+            </BaseForm>
+                
+        </>
     )
 }
 
